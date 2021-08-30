8c195335cb4f53ab2f5261bec4ff965b632b701c
Paul-Longo <imanazarite88@gmail.com>
2021-08-29 20:58:48 -0400

    Problem 16 Complete. Used Problem 15 outline to create the code for product price change in problem 16.

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -29,8 +29,8 @@ namespace DatabaseFirstLINQ
             //ProblemTwelve();
             //ProblemThirteen();
             //ProblemFourteen();
-            //ProblemFifteen();
-            //ProblemSixteen();
+            ProblemFifteen();
+            ProblemSixteen();
             //ProblemSeventeen();
             //ProblemEighteen();
             //ProblemNineteen();
@@ -234,7 +234,10 @@ namespace DatabaseFirstLINQ
         private void ProblemSixteen()
         {
             // Update the price of the product you created to something different using LINQ.
-
+            var product = _context.Products.Where(u => u.Name == "Sunflower Yeti Cup").SingleOrDefault();
+            product.Price = 35;
+            _context.Products.Update(product);
+            _context.SaveChanges();
         }
 
         private void ProblemSeventeen()
